105a751fcb76d0c93cff75207787eb2c45466c1a
Parth Lele <parthlele@Parths-MacBook-Pro.local>
2025-12-21 13:52:25 +0530

    changes

--- models/src/main/java/models/json/hotel/Room.java
+++ models/src/main/java/models/json/hotel/Room.java
@@ -9,9 +9,13 @@ import java.util.Map;
 @Data
 public class Room {
 
+    private String title;
+
     private RoomType roomType = RoomType.SINGLE;
 
     private Long numberOfRooms = 0L;
+
+    private Double currentPrice = 0D;
 
     // Map of date and price for a room
     private Map<String , Double> roomPricePerNightMap = new HashMap<>();
